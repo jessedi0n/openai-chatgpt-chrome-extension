792bb105ab76b1cea34861569da62ca1e0ed4bb3
Jesse <jesse.mahnken00@gmail.com>
2023-04-15 02:22:25 +0200

    - conversation context now working - background.js modified - refactoring

--- background.js
+++ background.js
@@ -1,34 +1,44 @@
+// on first install open the options page to set the API key
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason == "install") {
         chrome.tabs.create({ url: "options.html" });
     }
 });
 
-chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-    // clear the prevMessages storage when the popup is opened
-    chrome.storage.local.remove('prevMessages');
+// get the current time for context in the system message
+let time = new Date().toLocaleString('en-US');
 
+// create a system message
+const systemMessage = "You are a helpful chat bot. Your answer should not be too long. current time: " + time;
+
+// initialize the message array with a system message
+let messageArray = [
+    { role: "system", content: systemMessage }
+];
+
+// a event listener to listen for a message from the content script that says the user has openend the popup
+chrome.runtime.onMessage.addListener(function (request) {
+    // check if the request contains a message that the user has opened the popup
+    if (request.openedPopup) {
+        // reset the message array to remove the previous conversation
+        messageArray = [
+            { role: "system", content: systemMessage }
+        ];
+    }
 });
 
-chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-    if (request.query) {
-        // get the API key and previous messages from local storage
+// listen for a request message from the content script
+chrome.runtime.onMessage.addListener(async function (request) {
+    // check if the request contains a message that the user sent a new message
+    if (request.input) {
+        // get the API key from local storage
         let apiKey = await new Promise(resolve => chrome.storage.local.get(['apiKey'], result => resolve(result.apiKey)));
-        let prevMessages = await new Promise(resolve => chrome.storage.local.get(['prevMessages'], result => resolve(result.prevMessages)));
 
-        // add previous messages to the messages parameter
-        let messages = [];
-        if (prevMessages) {
-            messages = prevMessages.concat([{ "role": "user", "content": request.query }]);
-        } else {
-            messages = [
-                { "role": "system", "content": "you a are a helpful chat bot. your answer should not be too long." },
-                { "role": "user", "content": request.query }
-            ];
-        }
+        // Add the user's message to the message array
+        messageArray.push({ role: "user", "content": request.input });
 
         try {
-            // send the query to the OpenAI API
+            // send the request containing the messages to the OpenAI API
             let response = await fetch('https://api.openai.com/v1/chat/completions', {
                 method: 'POST',
                 headers: {
@@ -37,27 +47,37 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
                 },
                 body: JSON.stringify({
                     "model": "gpt-3.5-turbo",
-                    "messages": messages
+                    "messages": messageArray
                 })
             });
+
+            // check if the API response is ok Else throw an error
             if (!response.ok) {
                 throw new Error(`Failed to fetch. Status code: ${response.status}`);
             }
+
+            // get the data from the API response as json
             let data = await response.json();
-            console.log(data);
+
+            // check if the API response contains an answer
             if (data && data.choices && data.choices.length > 0) {
                 // get the answer from the API response
-                let answer = data.choices[0].message.content;
-                // send the answer back to the content script and store the previous messages
-                chrome.runtime.sendMessage({ answer: answer });
-                chrome.storage.local.set({ prevMessages: messages });
+                let response = data.choices[0].message.content;
+
+                // send the answer back to the content script
+                chrome.runtime.sendMessage({ answer: response });
+
+                // Add the response from the assistant to the message array
+                messageArray.push({ role: "assistant", "content": response });
             } else {
+                // send error message back to the content script
                 chrome.runtime.sendMessage({ answer: "No answer Found. Make sure your API-Key is valid." });
             }
         } catch (error) {
-            console.error("Error:", error);
-            chrome.runtime.sendMessage({ answer: `Error: ${error.message}` });
+            // send error message back to the content script
+            chrome.runtime.sendMessage({ answer: `Error: ${error}` });
         }
     }
+    // return true to indicate that the message has been handled
     return true;
 });
